7e0ae2daf0e70c5aebee019e4f53dbce4bc6d8aa
Veli-Pekka Luoto <jullesammakka@hotmail.fi>
2026-08-27 15:18:10 +0300

    unsafe off

--- healthapp/eslint.config.mjs
+++ healthapp/eslint.config.mjs
@@ -20,6 +20,7 @@ export default tseslint.config({
   rules: {
     '@stylistic/semi': 'error',
     //'@typescript-eslint/no-unsafe-assignment': 'error',
+    '@typescript-eslint/no-unsafe-argument': 'off',
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
